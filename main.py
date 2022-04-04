63422422d75900b73fb716aa9681c93ac4e103ae
majjikishore007 <majjikishore031@gmail.com>
2022-04-04 14:05:56 +0530

    updated

--- main.py
+++ main.py
@@ -30,15 +30,15 @@ class Scrapper:
 
     def get_brand(self, item):
         brand = item.find('h5', {'class': 's-line-clamp-1'})
-        return brand.get_text()
+        return brand.get_text() if brand else " "
 
     def get_name(self, item):
         name = item.find('h2', {'class': 's-line-clamp-2'})
-        return name.get_text()
+        return name.get_text() if name else " "
 
     def get_price(self, item):
         price = item.find('span', {'class': 'a-price-whole'})
-        return price.get_text()
+        return price.get_text() if price else " "
 
     def get_tat(self, item):
         tat = item.find(
@@ -47,60 +47,62 @@ class Scrapper:
         return list(tat.strings)[3].strip()
 
     def get_exact_page(self):
-        drop = self.driver.find_element(
-            By.NAME, value="dropdown_selected_size_name")
-        down = Select(drop)
-        time.sleep(3)
-        down.select_by_visible_text(self.size)
-        time.sleep(3)
+        try:
+            drop = self.driver.find_element(
+                By.NAME, value="dropdown_selected_size_name")
+            down = Select(drop)
+            time.sleep(3)
+            down.select_by_visible_text(self.size)
+            time.sleep(3)
 
-        location = self.driver.find_element(
-            By.XPATH, value='//*[@id="contextualIngressPtLabel_deliveryShortLine"]')
-        location.click()
-        time.sleep(3)
-        inp_text = self.driver.find_element(
-            By.XPATH, value=' //input[@id="GLUXZipUpdateInput"]')
-        inp_text.send_keys(self.pincode)
-        sub = self.driver.find_element(
-            By.XPATH, value='//*[@id="GLUXZipUpdate"]')
-        time.sleep(3)
-        sub.click()
-        time.sleep(2)
+            location = self.driver.find_element(
+                By.XPATH, value='//*[@id="contextualIngressPtLabel_deliveryShortLine"]')
+            location.click()
+            time.sleep(3)
+            inp_text = self.driver.find_element(
+                By.XPATH, value=' //input[@id="GLUXZipUpdateInput"]')
+            inp_text.send_keys(self.pincode)
+            sub = self.driver.find_element(
+                By.XPATH, value='//*[@id="GLUXZipUpdate"]')
+            time.sleep(3)
+            sub.click()
+            time.sleep(2)
 
-        suop = BeautifulSoup(self.driver.page_source, "html.parser")
+            suop = BeautifulSoup(self.driver.page_source, "html.parser")
 
-        return self.get_tat(suop)
+            return self.get_tat(suop)
+        except Exception as e:
+            return " "
         '''
         print("tat------->", get_tat(suop))
         brand,name , price ,url,tat
         []
         '''
 
-    def get_next_page(self):
-        print("going to the next page --------23444")
-        suop = BeautifulSoup(self.driver.page_source, "html.parser")
-        next_page = suop.find(
-            'a', {"class": "s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"})
-        print("link-----------", self.base+next_page['href'])
-        return self.base+next_page['href']
+    def get_next_page(self, suop):
+        next_p = suop.find(
+            'a', {"class": "s-pagination-next"})
+        print("next page ======", next_p)
+        return self.base+next_p['href']
 
-    def export_to_csv(self):
+    def export_to_csv(self, data):
         with open('products.csv', 'a', encoding='UTF8', newline='') as f:
             writer = csv.writer(f)
 
             # write the header
-            #writer.writerow(self.final_data)
+            # writer.writerow(self.final_data)
 
             # write multiple rows
-            writer.writerows(self.rows)
+            writer.writerows(data)
 
     def scrape_page(self):
         suop = BeautifulSoup(self.driver.page_source, "html.parser")
         results = suop.find_all(
             'div', {'data-component-type': "s-search-result"})
 
-        for i in range(2):
+        for i in range(len(results)):
             temp = []
+            rows = []
             url = self.get_item_data_link(results[i].find(
                 'span', {'data-component-type': 's-product-image'}))
 
@@ -110,27 +112,26 @@ class Scrapper:
             temp.append(url)
             self.driver.get(url)
             temp.append(self.get_exact_page())
-            self.rows.append(temp)
-
-        self.export_to_csv()
+            rows.append(temp)
+            self.export_to_csv(rows)
 
     def main(self):
         try:
+
             self.driver.get(self.get_url(self.product))
             time.sleep(3)
-
+            suop = BeautifulSoup(self.driver.page_source, "html.parser")
             # first page
             self.scrape_page()
 
             # starts from 2nd page if exits
 
             for i in range(2):
-                print("going to the next page----------------")
+                self.driver.get(self.get_next_page(suop))
                 self.rows = []
-                self.driver.get(self.get_next_page())
                 time.sleep(3)
+                suop = BeautifulSoup(self.driver.page_source, "html.parser")
                 self.scrape_page()
-
         except Exception as e:
             print(e)
         '''
